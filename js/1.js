7f7b4140db6ca3a2008ff7668b01c17078a21fb9
Gladiator04n73 <70691711+Gladiator04n73@users.noreply.github.com>
2023-06-02 01:54:10 +0300

    1

--- js/1.js
+++ js/1.js
@@ -80,3 +80,20 @@ function simple() {
     }
 }
 
+function catdog() {
+    let num = prompt("Введите количество строк: ");
+    let k = Math.floor(num / 5);
+    let l = num - 5 * k;
+    let one = "dog dog dog cat cat dog";
+    let two = "dog dog cat cat dog dog";
+    let three = "dog cat cat dog dog dog";
+    let four = "cat cat dog dog dog dog";
+    let five = "cat dog dog dog dog cat";
+    var list = [one, two, three, four, five]
+    for (let i = 1; i <= k; i++) {
+        for (let j = 0; j < 5; j++) console.log(list[j]);
+    }
+    for (let j = 0; j < l; j++) console.log(list[j]);
+}
+
+
