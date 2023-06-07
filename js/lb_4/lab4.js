3f2a58182f49aecc81d44568c298dc89037d20c0
Gladiator04n73 <70691711+Gladiator04n73@users.noreply.github.com>
2023-06-07 09:56:05 +0300

    1

--- js/lb_4/lab4.js
+++ js/lb_4/lab4.js
@@ -68,7 +68,10 @@ function adClas(){
               return this;
             }
         };
-     }
+        console.log( obj.addClass('new')); 
+        console.log(obj.removeClass('open')); 
+        console.log(obj.removeClass('menu'));
+    }
 function people(){
     console.clear()
     let vasya = { name: "Вася", age: 23 };
@@ -77,6 +80,26 @@ let vova = { name: "Вова", age: 12 };
 let people = [ vasya , masha , vova ];
 people.sort((a, b) => a.age - b.age);
 console.log(people);
+}
+function datas(){
+    function getSecondsToday() {// функция для получения количества секунд с начала текущего дня
+        let now = new Date(); // получаем текущую дату и время
+        let start = new Date(now.getFullYear(), now.getMonth(), now.getDate()); // создаем новую дату, устанавливая время в начало текущего дня
+        let diff = now - start; // разница между текущей датой и началом дня в миллисекундах
+        let seconds = Math.floor(diff / 1000); // переводим разницу в секунды и округляем до целого числа
+        return seconds;
+      }
+      function formatDate(date) { // функция для форматирования даты
+        let day = date.getDate();
+        if (day < 10) day = '0' + day; // добавляем ведущий ноль, если число меньше 10
+        let month = date.getMonth() + 1;
+        if (month < 10) month = '0' + month; // добавляем ведущий ноль, если число меньше 10
+        let year = date.getFullYear() % 100; // получаем две последние цифры года
+        if (year < 10) year = '0' + year; // добавляем ведущий ноль, если число меньше 10
+        return day + '.' + month + '.' + year;
+      } 
+      console.log(getSecondsToday());
+      console.log(formatDate(new Date()));
 }
 function getSecondsToday() {// функция для получения количества секунд с начала текущего дня
     let now = new Date(); // получаем текущую дату и время
