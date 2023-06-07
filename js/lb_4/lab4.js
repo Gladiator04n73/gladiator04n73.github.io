3c58db89ea2f9fd647139a2863e000a94d1874ca
Gladiator04n73 <70691711+Gladiator04n73@users.noreply.github.com>
2023-06-07 09:43:08 +0300

    1

--- js/lb_4/lab4.js
+++ js/lb_4/lab4.js
@@ -43,3 +43,51 @@ function books(){
     console.log(isEmpty(book2));
     console.log(isEmpty(book3));
         }
+function adClas(){
+            let obj = {
+            className: 'open menu',
+        
+            addClass(cls) {
+              let classes = this.className.split(' ');
+              if (!classes.includes(cls)) {
+                classes.push(cls);
+                this.className = classes.join(' ');
+              }
+              return this;
+            },
+          
+            removeClass(cls) {
+              let classes = this.className.split(' ');
+              let index = classes.indexOf(cls);
+              if (index !== -1) {
+                classes.splice(index, 1);
+                this.className = classes.join(' ');
+              }
+              return this;
+            }
+        };
+     }
+function people(){
+    let vasya = { name: "Вася", age: 23 };
+let masha = { name: "Маша", age: 18 };
+let vova = { name: "Вова", age: 12 };
+let people = [ vasya , masha , vova ];
+people.sort((a, b) => a.age - b.age);
+console.log(people);
+}
+function getSecondsToday() {// функция для получения количества секунд с начала текущего дня
+    let now = new Date(); // получаем текущую дату и время
+    let start = new Date(now.getFullYear(), now.getMonth(), now.getDate()); // создаем новую дату, устанавливая время в начало текущего дня
+    let diff = now - start; // разница между текущей датой и началом дня в миллисекундах
+    let seconds = Math.floor(diff / 1000); // переводим разницу в секунды и округляем до целого числа
+    return seconds;
+  }
+  function formatDate(date) { // функция для форматирования даты
+    let day = date.getDate();
+    if (day < 10) day = '0' + day; // добавляем ведущий ноль, если число меньше 10
+    let month = date.getMonth() + 1;
+    if (month < 10) month = '0' + month; // добавляем ведущий ноль, если число меньше 10
+    let year = date.getFullYear() % 100; // получаем две последние цифры года
+    if (year < 10) year = '0' + year; // добавляем ведущий ноль, если число меньше 10
+    return day + '.' + month + '.' + year;
+  }
